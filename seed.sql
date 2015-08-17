98cc097c904d094d486a07909dbddcc263a2ae5c
Alexander Marino <marinoalexp@gmail.com>
2015-08-17 08:47:24 -0400

    Do commit

--- seed.sql
+++
@@ -1,8 +0,0 @@
-
-INSERT INTO topics (title, username, content, tags, votes) VALUES ("The Matrix", "apmarino", "Lorem Ipsum", " new, movies, scifi ", 2005);
-INSERT INTO topics (title, username, content, tags, votes) VALUES ("The Fifth Element", "Wacko", "Lorem Ipsum", " new, awesome ", 20);
-INSERT INTO topics (title, username, content, tags, votes) VALUES ("Star Wars", "Dot", "Lorem Ipsum", " new, movie, nerd ", 15);
-
-INSERT INTO comments (username, topic_id, content) VALUES ("apmarino", 1, "This movie is awesome");
-INSERT INTO comments (username, topic_id, content) VALUES ("Wacko", 1, "He is the one");
-INSERT INTO comments (username, topic_id, content) VALUES ("Dot", 1, "I don't beleive it");
